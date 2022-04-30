b2b7db04996bf8cae50d98827a2b5ae648e5bc41
Aayush Ghosh <86785890+SpideR1sh1@users.noreply.github.com>
2022-04-30 01:47:24 -0400

    Time: 0 ms (100.00%), Space: 6.2 MB (93.60%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,6 +1,21 @@
 class Solution {
-    public:
+public:
     bool isMatch(string s, string p) {
-        return regex_match(s,regex(p));
+        int m = s.size(), n = p.size();
+        vector<bool> cur(n + 1, false);
+        for (int i = 0; i <= m; i++) {
+            bool pre = cur[0];
+            cur[0] = !i;
+            for (int j = 1; j <= n; j++) {
+                bool temp = cur[j];
+                if (p[j - 1] == '*') {
+                    cur[j] = cur[j - 2] || (i && cur[j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
+                } else {
+                    cur[j] = i && pre && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+                }
+                pre = temp;
+            }
+        }
+        return cur[n];
     }
 };
